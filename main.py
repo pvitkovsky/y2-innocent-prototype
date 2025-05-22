82af5f13cd9732468ffe71a645942a473089a105
Vitkovsky <philipp@pangeabiomed.com>
2025-05-22 17:29:01 +0300

    more queries

--- main.py
+++ main.py
@@ -17,30 +17,52 @@ if __name__ == "__main__":
     searches: List[dict] = [
         {
             "key": "dev_query_7k",
-            "name": "Up To 7K",  # print name
+            "name": "Local - Up To 7K", 
             "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=6000&maxPrice=7000&minRooms=2&maxRooms=3&property=1&balcony=1&multiCity=8700,6400,6900,9700"
         },
         {
             "key": "dev_query_6k",
-            "name": "Up To 6K",  # print name
+            "name": "Local - Up To 6K", 
             "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=5000&maxPrice=6000&minRooms=2&maxRooms=3&property=1&balcony=1&multiCity=8700,6400,6900,9700"
         },
         {
             "key": "dev_query_5k",
-            "name": "Up To 5K",  # print name
+            "name": "Local - Up To 5K", 
             "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=4000&maxPrice=5000&minRooms=2&maxRooms=3&property=1&balcony=1&multiCity=8700,6400,6900,9700"
         },
         {
             "key": "dev_query_4k",
-            "name": "Up To 4K",  # print name
-            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=3000&maxPrice=4000&minRooms=2&maxRooms=3&property=1&balcony=1&multiCity=8700,6400,6900,9700"
+            "name": "Local - Up To 4K", 
+            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=3000&maxPrice=4000&minRooms=2&maxRooms=3&property=1&balcony=1&multiNeighborhood=1462&multiCity=8600,6300"
+        },
+
+
+        {
+            "key": "rg_query_7k",
+            "name": "Ramat Gan - Up To 7K", 
+            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=6000&maxPrice=7000&minRooms=2&maxRooms=3&property=1&balcony=1&multiNeighborhood=1462&multiCity=8600,6300"
+        },
+        {
+            "key": "rg_query_6k",
+            "name": "Ramat Gan - Up To 6K", 
+            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=5000&maxPrice=6000&minRooms=2&maxRooms=3&property=1&balcony=1&multiNeighborhood=1462&multiCity=8600,6300"
+        },
+        {
+            "key": "rg_query_5k",
+            "name": "Ramat Gan - Up To 5K", 
+            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=4000&maxPrice=5000&minRooms=2&maxRooms=3&property=1&balcony=1&multiNeighborhood=1462&multiCity=8600,6300"
+        },
+        {
+            "key": "rg_query_4k",
+            "name": "Ramat Gan - Up To 4K", 
+            "url": "https://gw.yad2.co.il/realestate-feed/rent/map?minPrice=3000&maxPrice=4000&minRooms=2&maxRooms=3&property=1&balcony=1&multiNeighborhood=1462&multiCity=8600,6300"
         }
     ]
 
     for search in searches:
         query_name = search['key']
         state = client.get_state(query_name)
-        apartments: List[Apartment] = fetcher.fetch_and_parse(name=search['name'].lower(), url=search['url'], fetch=False)
+        apartments: List[Apartment] = fetcher.fetch_and_parse(name=search['name'].lower(), url=search['url'], fetch=True)
         synchronizer = SyncInstance(query_name)
         ingested = synchronizer.sync(apartments, state) # TODO: doesn't do anything archived;
         client.delete(query_name)
