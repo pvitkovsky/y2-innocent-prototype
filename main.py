e19aadf3c88bd0930f08112107754797c66ee8e9
shtirlitz <vitkovsky1337@gmail.com>
2025-09-08 17:28:34 +0300

    as image d/l merged

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 from typing import List
 
+from src.apartament_img import ApartamentImageDownloader
 from src.supa_client import SupaClient, ApartamentQuery, SupaState
 from src.sync_instance import SyncInstance
 from src.y2_ingest_svc import Apartment, Y2Fetcher, IngestedApartament
 
-if __name__ == "__main__":
+def update_portal(searches: List[dict]):
 
     client = SupaClient(
         queries_url='https://kbbcllgitrzhwbyfgevc.supabase.co/rest/v1/queries',
@@ -13,6 +14,30 @@ if __name__ == "__main__":
         supabase_key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiYmNsbGdpdHJ6aHdieWZnZXZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5Nzg1NzksImV4cCI6MjA1ODU1NDU3OX0.acHkhagTOFGU88812SKyZe39nG4SM_9MpSmEIBMIH6w'
     )
     fetcher = Y2Fetcher()
+
+
+    for search in searches:
+        query_name = search['key']
+        state = client.get_state(query_name)
+        apartments: List[Apartment] = fetcher.fetch_and_parse(name=search['name'].lower(), url=search['url'], fetch=True)
+        synchronizer = SyncInstance(query_name)
+        ingested = synchronizer.sync(apartments, state)
+        client.delete(query_name)
+        client.ingest_values(ingested)
+
+        missing: List[SupaState] = synchronizer.check_missing(apartments, state)
+        favs_missing = list(filter(lambda a: a.score is not None, missing))
+        print(f"Missing apartaments {len(missing)}, "
+                           f" favourites missing: {len(favs_missing)}")
+        if len(favs_missing) > 0:
+            synchronizer.extended_print(favs_missing)
+
+
+    client.ingest_queries([ApartamentQuery(search['key'], search['name'], search['url']) for search in searches])
+
+
+if __name__ == "__main__":
+
 
     searches: List[dict] = [
         {
@@ -59,19 +84,9 @@ if __name__ == "__main__":
         }
     ]
 
+    update_portal(searches)
     # TODO: solve cascade problem; ingesting dumps the cascade
-    for search in searches:
-        query_name = search['key']
-        state = client.get_state(query_name)
-        apartments: List[Apartment] = fetcher.fetch_and_parse(name=search['name'].lower(), url=search['url'], fetch=False)
-        synchronizer = SyncInstance(query_name)
-        ingested = synchronizer.sync(apartments, state)
-        client.delete(query_name)
-        client.ingest_values(ingested)
-
-
-    client.ingest_queries([ApartamentQuery(search['key'], search['name'], search['url']) for search in searches])
-
     # TODO: consider having at least launch args to fetch or dry run; as well, cron job on my gaming pc to sync;
     # TODO: consider if can do w Curl and not selenium to make this deployed
+    # TODO: consider getting the descriptions;
 
